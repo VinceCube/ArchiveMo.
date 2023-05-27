fadbbc737cb335a7871fd2090109fe71ae2cd922
VinceCube <130943095+VinceCube@users.noreply.github.com>
2023-05-27 13:17:20 +0800

    A

--- index.php
+++ index.php
@@ -5,10 +5,25 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ArchiveMo</title>
-    
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <div class="login_section">
+        <div class="row">
+        <div class="col">
+            <img src="img/login_pic.png" alt="">
+        </div>
+        <div class="col">
+            <form action="">
+                <h1>Welcome!</h1>
+                <h4>Login to continue</h4>
+                <input type="email" name="email" placeholder="Useremail">
+                <input type="password" name="password" placeholder="Password">
+                <input type="submit" name="submit" value="Login">
+            </form>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
 
